0f0ff7f88de9a433a916a9e944612089093a77ec
前端小学生OvO <51734090+fanzhikang0320@users.noreply.github.com>
2020-08-09 08:32:24 +0800

    Antisnoring v1.0

---
+++ src/components/wifi-product.vue
@@ -0,0 +1,237 @@
+<template>
+  <div class="product-wrapper">
+      <div :class="{'product-item': true, 'first-item': index == 0, 'last-item': index == productList.length - 1}" v-for="(product,index) in productList" :key="index">
+          <div class="circle" v-if="index != productList.length- 1">
+              <span class="num">{{index + 1}}</span>
+          </div>
+          <div class="corner-box" v-if="index == 0">
+              <span class="text">50% Discount Today!</span>
+          </div>
+          <div class="big-title" v-if="index == productList.length - 1">The Most Popular Anti-Snoring Device In 2020</div>
+          <a :href="product.link" @click.stop="handleExecute" target="_blank" class="product-link-box">
+            <div class="product-container">
+                <div class="product-container-left">
+                    <div class="picture-box">
+                        <img :src="product.picture" :alt="product.name" class="picture">
+                    </div>
+                    <div class="desc-list-box">
+                        <h2 class="product-name">{{product.name}}</h2>
+                        <ul class="list-box" v-if="product.strengthList.length != 0">
+                            <li v-for="(item,id) in product.strengthList" :key="id">
+                                <span class="icon"></span>
+                                <p class="text">{{item.text}}</p>
+                            </li>
+                        </ul>
+                        <ul class="list-box weakness-list" v-if="product.weaknessList.length != 0">
+                            <li v-for="(item,id) in product.weaknessList" :key="id">
+                                <span class="icon"></span>
+                                <p class="text">{{item.text}}</p>
+                            </li>
+                        </ul>
+                        <p class="buys" v-if="index == 0 || index == productList.length - 1">Buy 1 Get 1 Free NOW</p>
+                    </div>
+                </div>
+                <div class="product-container-right">
+                    <p class="good">
+                        <span class="text">{{index == 0 || index == productList.length - 1 ? 'Excellent': index == 1 ? 'Very Good': 'Good'}}</span>
+                        <span class="king-icon" v-if="index == 0 || index == productList.length - 1"></span>
+                        <span class="good-icon" v-if="index == 1"></span>
+                    </p>
+                    <p class="rate-box"><span class="score">{{product.score}}</span><span>/10</span></p>
+                    <a :href="product.link" @click.stop="handleExecute" target="_blank" class="btn">
+                        <span class="text">Visit Site</span>
+                        <span class="icon"></span>
+                    </a>
+                    <!-- <a :href="product.reviewLink" class="review">Read Our Review</a> -->
+                </div>
+            </div>
+          </a>
+      </div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            productList: [
+                {
+                    id: 1,
+                    name: 'StopSnore Clip',
+                    picture: require('../assets/image/snoring/stopSnoreClip1.png'),
+                    link: 'https://www.esplma.com/7QLQCMW/59DPXX5/?uid=6865',
+                    reviewLink: '',
+                    buys: 586,
+                    strengthList: [
+                        {
+                            text: 'Best Anti Snoring Device In The Market'
+                        },
+                        {
+                            text: 'Eco-Friendly 100% Silicone Gel'
+                        },
+                        {
+                            text: 'Super Easy To Use'
+                        },
+                        {
+                            text: 'Solves Snoring Once And For All'
+                        },
+                        {
+                            text: 'Easy To Clean And Reusable'
+                        },
+                        {
+                            text: 'Helps Reduce Dry Mouth'
+                        },
+                        {
+                            text: 'Extremely Low Price'
+                        }
+                    ],
+                    weaknessList: [],
+                    score: '10'
+
+                },
+                {
+                    id: 2,
+                    name: 'Sleep Connection',
+                    picture: require('../assets/image/snoring/sleepConnection.png'),
+                    link: 'https://www.edlwss.com/7QLQCMW/4GFQX62/?uid=4168',
+                    reviewLink: '',
+                    buys: 586,
+                    strengthList: [
+                        {
+                            text: 'Stop Snoring From The First Night'
+                        },
+                        {
+                            text: 'Just Put On the Wrist And It Starts'
+                        },
+                        {
+                            text: 'Initiate With Single Button Press'
+                        },
+                        {
+                            text: 'Automatic Power Off After 8 Hours'
+                        },
+                        {
+                            text: 'Lightweight, Non-Invasive Wearable Technology'
+                        }
+                    ],
+                    weaknessList: [
+                        {
+                          text: 'Higher price than other competitors'  
+                        }
+                    ],
+                    score: '9.6'
+
+                },
+                {
+                    id: 3,
+                    name: 'Snore Strap',
+                    picture: require('../assets/image/snoring/snoreStrap.png'),
+                    link: 'https://hyperstech.com/intl/order.php?prod=snorestrap&net=9785&aff={AFFID}&sid={SUBID}&cid={CLICKID}',
+                    reviewLink: '',
+                    buys: 586,
+                    strengthList: [
+                        {
+                            text: 'Helps You and Your Partner to Have a Better Sleep'
+                        },
+                        {
+                            text: 'Wake up Refreshed without Fatigue'
+                        },
+                        {
+                            text: 'Designed for Easy Daily Use'
+                        },
+                        {
+                            text: 'Supports with Jaw Dislocation Problem'
+                        },
+                        {
+                            text: 'Safe and Comfortable, High Quality Materials'
+                        },
+                        {
+                            text: 'Keeps airways open'
+                        }
+                    ],
+                    weaknessList: [],
+                    score: '9.5'
+
+                },
+                {
+                    id: 4,
+                    name: 'SilentSnore',
+                    picture: require('../assets/image/snoring/silentSnore.png'),
+                    link: 'https://hypertechx.com/ps/silentsnore/index.php?net=9785&aff={AFFID}&sid={SUBID}&cid={CLICKID}',
+                    reviewLink: '',
+                    buys: 586,
+                    strengthList: [
+                        
+                        {
+                            text: 'One-Size-Fits-All'
+                        },
+                        {
+                            text: 'Soft & Light Design'
+                        },
+                        {
+                            text: 'Stimulates The Sensory Nerves'
+                        },
+                        {
+                            text: 'Non-Intrusive & Comfortable'
+                        },
+                        {
+                            text: 'Made FromHigh-Quality & Non-Toxic Materials'
+                        }
+                    ],
+                    weaknessList: [
+                        {
+                            text: 'Expensive For Value'
+                        }
+                    ],
+                    score: '9.2'
+
+                },
+                {
+                    id: 1,
+                    name: 'StopSnore Clip',
+                    picture: require('../assets/image/snoring/stopSnoreClip2.png'),
+                    link: 'https://www.esplma.com/7QLQCMW/59DPXX5/?uid=6865',
+                    reviewLink: '',
+                    buys: 586,
+                    strengthList: [
+                        {
+                            text: 'Best Anti Snoring Device In The Market'
+                        },
+                        {
+                            text: 'Eco-Friendly 100% Silicone Gel'
+                        },
+                        {
+                            text: 'Super Easy To Use'
+                        },
+                        {
+                            text: 'Solves Snoring Once And For All'
+                        },
+                        {
+                            text: 'Easy To Clean And Reusable'
+                        },
+                        {
+                            text: 'Helps Reduce Dry Mouth'
+                        },
+                        {
+                            text: 'Extremely Low Price'
+                        }
+                    ],
+                    weaknessList: [],
+                    score: '10'
+
+                }
+            ]
+        }
+    },
+    methods: {
+        handleExecute() {
+            window.execute();
+        }
+    }
+
+}
+</script>
+
+<style lang="less" scoped>
+@import url('../assets/css/wifi-product.less');
+</style>
---
+++ src/views/Snoring.vue
@@ -0,0 +1,259 @@
+<template>
+  <div class="mosquito-wrapper">
+        <div class="maskLayer">
+                <div class="mask-content ">
+                    <span class="close"></span>
+                    <a :href="mainLink"  @click.stop="handleExecute" target="_blank" class="mask-box clearfix">
+                        <div class="mask-left">
+                            <img src="@/assets/image/snoring/alert-ad.jpg" alt="" class="ad">
+                        </div>
+                        <div class="mask-right">
+                            <p class="offer">Limited Time Offer</p>
+                            <p class="save"><span>SAVE</span><strong>50% MONEY</strong></p>
+                            <p class="OEI">Offer Expires In</p>
+                            
+                            <div class="date-area">
+                                <div class="time-box colon">
+                                    <span class="text">Hours</span>
+                                    <div class="time">
+                                        <span class="hours-decade">0</span>
+                                        <span class="hours-unit">0</span>
+                                    </div>
+                                </div>
+                                <div class="time-box colon">
+                                    <span class="text">Minutes</span>
+                                    <div class="time">
+                                        <span class="minutes-decade">0</span>
+                                        <span class="minutes-unit">0</span>
+                                    </div>
+                                </div>
+                                <div class="time-box">
+                                    <span class="text">Seconds</span>
+                                    <div class="time">
+                                        <span class="seconds-decade">0</span>
+                                        <span class="seconds-unit">0</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="desc">30 Days Money Back Guarantee</p>
+                            <a :href="mainLink"  @click.stop="handleExecute" target="_blank" rel="noopener noreferrer" class="btn">Get the discount today!</a>
+                        </div>
+                    </a>
+                </div>
+        </div>
+      <header class="mosquito-header">
+          <div class="header-left">
+                <img src="@/assets/image/logo.png" alt="SMART CHOICES" class="logo">
+                <p class="text">Provides Comfortable, Effective Way to Stop Snoring</p>
+          </div>
+          <a href="/" class="btn">Anti-Snoring Devices</a>
+
+      </header>
+      <section class="banner">
+          <div class="banner-container">
+               <div class="banner-content">
+                    <h1 class="g">2020’s TOP 4</h1> 
+                    <h1 class="a">Anti-Snoring Devices</h1>
+                </div>
+          </div>
+          
+      </section>
+      <main class="container">
+          <div class="desc-area">
+              <h2 class="area-title">The Best Anti-Snoring Devices In 2020</h2>
+              <div class="desc-content">
+                  <p>Millions of people are familiar with it - snoring. The number one annoyance in the bedroom at night. About one in two men and one in four women of middle age or older snores in their sleep. Previously dismissed as just an annoying disturbance of the peace, snoring is now recognized as a serious sleep and health disorder. Fortunately, a revolutionary solution is now available.</p>
+                    <p>We tested some of the most popular anti snoring devices on the market. Our researchers looked at price, product quality, ease of use, scope, effectiveness, and discount. </p>
+              </div>
+          </div>
+          <div class="product-area">
+              <p class="product-area-title">And then we sorted out 4 major options:</p>
+              <WifiProduct/>
+          </div>
+          <div class="why-area">
+              <h2 class="area-title">Why Do We Need Anti-Snoring Devices?</h2>
+              <div class="why-container">
+                  <div class="img-box">
+                      <div class="img-item">
+                          <img src="@/assets/image/snoring/huanbao.svg" alt="" class="svg">
+                          <span class="text">portable and reusable</span>
+                      </div>
+                      <div class="img-item">
+                          <img src="@/assets/image/snoring/voice.svg" alt="" class="svg">
+                          <span class="text">effectively stop snoring</span>
+                      </div>
+                      <div class="img-item">
+                          <img src="@/assets/image/snoring/moon.svg" alt="" class="svg">
+                          <span class="text">safe and comfortable</span>
+                      </div>
+                  </div>
+                  <div class="content">
+                      <p>There are many short-term and long-term factors that can cause snoring and require people to seek out anti-snoring devices. Snoring can be something that happens on occasion, as well as something that is a chronic problem. For example, it can be caused by everything from <strong>mouth anatomy, to alcohol consumption, to nasal problems, to sleep position, to sleep deprivation, to congestion, to allergies</strong>. Men are also more susceptible to snoring problems than women. Although snoring is a bothersome problem to have, we’re lucky that there are so many great anti-snoring devices options on the market! Our top anti-snoring device pick, the StopSnore Clip, has helped countless snorers fix their snoring problems once and for all.</p>
+                  </div>
+              </div>
+          </div>
+          <div class="what-area">
+              <h2 class="area-title">What Is The Most Effective Anti Snoring Device?</h2>
+              <div class="content">
+                  <p>The battle for best anti-snoring device was tough, but the <strong>StopSnore Clip</strong> came out on top, without a doubt. This device is particularly special because it’s so <strong>small, comfortable and non-intrusive</strong>. One of the biggest complaints that we hear about anti-snoring devices in general is that they’re bothersome and uncomfortable to wear. Now, you can move around freely while you sleep and even cuddle your partner easily while wearing your anti-snoring device at the same time. The StopSnore Clip uses the <strong>best of technology</strong> in order to help people with their snoring problems. The use of sensitive biosensors is impressive and non-invasive. This incredible anti-snoring device enables people to overcome their snoring problems without even being aware of it! All in all, we give this product a strong 10/10. </p>
+              </div>
+          </div>
+          <div class="how-area">
+              <h2 class="area-title">How StopSnore Clip Works?</h2>
+              <div class="how-container">
+                    <p>The magnets in StopSnore gently apply pressure in the nose, widening nostrils. This allows air to flow through the nose. When this happens, breathing starts to take place with ease through the nose. With the air moving easier and more regularly, the throat vibrations mentioned earlier are reduced or eliminated.</p>
+                    <div class="sample-box">
+                        <div class="sample-item">
+                            <img src="@/assets/image/snoring/sample1.jpg" alt="" class="picture">
+                            <p class="content">Simply apply StopSnore into the nasal opening before going to sleep. Gentle pressure allows nasal breathing.</p>
+                        </div>
+                        <div class="sample-item">
+                            <img src="@/assets/image/snoring/sample2.jpg" alt="" class="picture">
+                            <p class="content">Breathe again - StopSnore promotes nasal breathing which reduces or eliminates snoring.</p>
+                        </div>
+                    </div>
+                    
+                    <p>This amazing invention works the whole night, without any drugs and without interrupting sleep. Better sleep can lead to wonderful benefits. It can improve your mood, health, energy and performance.</p>
+                    <p>A normal and healthy sleep cycle should include at least 25% at deep sleep, by relieving snoring effectively, stop disturbing your sleeping partner immediately, and guarantee your body rest and regenerate well.</p>
+              </div>
+          </div>
+          <div class="special-area">
+              <div class="special-container">
+                    <h3 class="offer">Special Offer on Now!</h3>
+                    <p class="sub">Now there is a special offer, but such a low price, there may not be much left! </p>
+                    <p class="sub">So do it now!</p>
+                    <p class="get-up">Get Up To <span class="big-red">50% OFF</span> Your <strong>StopSnore Clip</strong> Today!</p>
+                    <img src="@/assets/image/snoring/special-ad.jpg" alt="" class="picture">
+                    <a :href="mainLink" @click="handleExecute" target="_blank" rel="noopener noreferrer" class="btn">Get Your StopSnore Clip Now!</a>
+                </div>
+          </div>
+          <div class="faq-area">
+              <h2 class="area-title">Tips & Ideas to Use Alongside Anti-Snoring Devices</h2>
+              <div class="question-box">
+                  <div class="question-item" v-for="(item,index) in questionData" :key="index">
+                      <!-- <h3 class="question-title">{{item.title}}</h3> -->
+                      <div class="question-content" v-html="item.content"></div>
+                  </div>
+              </div>
+          </div>
+      </main>
+  </div>
+</template>
+
+<script>
+import WifiProduct from '@/components/wifi-product.vue'
+import $ from 'jquery'
+export default {
+    components: {
+        WifiProduct
+    },
+    data() {
+        return {
+            mainLink: 'https://www.esplma.com/7QLQCMW/59DPXX5/?uid=6865',
+            questionData: [
+                {
+                    title: 'Tips & Ideas to Use Alongside Anti-Snoring Devices',
+                    content: `<p>There are many little things you can try doing to take care of your snoring problems alongside using an anti-snoring device. You could change the position you sleep in and try sleeping on your side or sleeping with your head raised. Using the right type of pillow can help with this. Making healthier choices such as limiting alcohol consumption, not smoking, getting enough sleep, drinking more water and cutting down on inflammatory foods will also help significantly. Treat chronic allergies with the help of your doctor and avoid sleeping pills too as they can actually cause snoring. Many snorers experience even faster success when trying these strategies alongside their anti-snoring device. Good luck and sleep well!</p>`
+                }
+            ],
+            timer: null,
+            count: 0
+        }
+    },
+    methods: {
+        /**
+         * 倒计时
+         */
+        countDown() {
+            //结束时间
+            let finishTime = new Date('2020/7/31').getTime();
+            let startTime = new Date(new Date().toLocaleDateString()).getTime();
+            let intervalTime = 0,
+                day = 0,
+                hours = 0,
+                minutes = 0,
+                seconds = 0;
+            let that = this;
+            this.timer = setInterval(function () {
+                if (finishTime - new Date().getTime() <= 0) {
+                     finishTime = new Date(new Date().toLocaleDateString()).getTime() + 1 * 24 * 60 * 60 * 1000;
+                }
+                intervalTime = finishTime - new Date().getTime();
+
+                day =  Math.floor(intervalTime/(1000*60*60*24));
+                hours = Math.floor(intervalTime / (60 * 60 * 1000) % 24) + day * 24;
+                minutes = Math.floor(intervalTime / (60 * 1000) % 60);
+                seconds = Math.floor(intervalTime / 1000 % 60); 
+                that.renderTime(hours,minutes,seconds);
+                
+            },1000)
+            
+        },
+        renderTime(hours,minutes,seconds) {
+            let hoursObj = this.splitCell(hours);
+            let minutesObj = this.splitCell(minutes);
+            let secondsObj = this.splitCell(seconds);
+            $('.hours-decade').text(hoursObj.decade);
+            $('.hours-unit').text(hoursObj.unit);
+            $('.minutes-decade').text(minutesObj.decade);
+            $('.minutes-unit').text(minutesObj.unit);
+            $('.seconds-decade').text(secondsObj.decade);
+            $('.seconds-unit').text(secondsObj.unit);
+
+        },
+        splitCell(num) {
+            let unit = Math.floor(num % 10);
+            let decade = Math.floor(num / 10);
+            return {
+                unit: unit,
+                decade: decade
+            }
+        },
+        alert() {
+            $('.maskLayer').css({
+                display: 'block'
+            })
+            $('.mask-content').animate({opacity: 1,top: '50%'},500)
+        },
+        handleExecute() {
+            window.execute();
+        }
+    },
+    mounted() {
+        this.countDown();
+        let that = this;
+        $('body').on('mouseenter',function () {
+            $('body').one('mouseleave',function () {
+                if (that.count == 0) {
+
+                    that.alert();
+                }
+                that.count ++;
+            })
+        })
+        $('.close').on('click',function (event) {
+            event.stopPropagation();
+            $('.maskLayer').css({
+                display: 'none'
+            })
+            clearInterval(that.timer);
+        })
+        $('.maskLayer').on('click',function (e) {
+            e.stopPropagation();
+            $('.maskLayer').css({
+                display: 'none'
+            })
+            clearInterval(that.timer);
+        })
+    },
+    beforeDestroy() {
+        $('body').off('mouseenter');
+        clearInterval(this.timer);
+        this.timer = null;
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import url('../assets/css/Photo.less');
+</style>
